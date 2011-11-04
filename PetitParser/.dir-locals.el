315f4cf31da511096a83a60ab10b271e9c804e5d
Damien Cassou <damien@cassou.me>
2011-11-04 17:06:45 +0000

    Working on PetitParser
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>
    
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

---
+++ PetitParser/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((latex-mode
+  (ispell-local-dictionary . "english")
+  (TeX-PDF-mode . t)
+  (TeX-master . "PetitParser")
+  (coding . utf-8)))
